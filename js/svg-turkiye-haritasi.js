4aa34bd3b380201d5fd7c1b72b20c749a2fa2e7b
Batuhan Hasan AKIN <batu@Batuhan-MacBook-Pro.local>
2019-11-02 19:00:23 +0300

    GDG icin revize edildi.

--- js/svg-turkiye-haritasi.js
+++ js/svg-turkiye-haritasi.js
@@ -3,31 +3,35 @@
 function svgturkiyeharitasi() {
   const element = document.querySelector('#svg-turkiye-haritasi');
   const info = document.querySelector('.il-isimleri');
+  const imge = document.querySelector('.imge');
 
   element.addEventListener(
     'mouseover',
     function (event) {
-      if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris') {
+      if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris' && event.target.parentNode.className.baseVal === 'gdg') {
+        imge.src ='./gdg-logo.png';
         info.innerHTML = [
           '<div>',
           event.target.parentNode.getAttribute('data-iladi'),
-          '</div>'
+          '</div>',
         ].join('');
       }
     }
   );
-
   element.addEventListener(
     'mousemove',
     function (event) {
+      imge.style.top = event.pageY + 32 + 'px';
+      imge.style.left = event.pageX  + 'px';
       info.style.top = event.pageY + 25 + 'px';
-      info.style.left = event.pageX + 'px';
+      info.style.left = event.pageX +20+ 'px';
     }
   );
 
   element.addEventListener(
     'mouseout',
     function (event) {
+      imge.src = '';
       info.innerHTML = '';
     }
   );
@@ -35,21 +39,17 @@ function svgturkiyeharitasi() {
   element.addEventListener(
     'click',
     function (event) {
-      if (event.target.tagName === 'path') {
+      if (event.target.tagName === 'path' && event.target.parentNode.className.baseVal === 'gdg') {
         const parent = event.target.parentNode;
         const id = parent.getAttribute('id');
 
-        if (
-          id === 'guney-kibris'
-        ) {
+        if (id === 'guney-kibris' ) {
           return;
         }
-
         window.location.href = (
-          '#'
+          'https://www.google.com/search?q=gdg+'
           + id
-          + '-'
-          + parent.getAttribute('data-plakakodu')
+          + ' meetup'
         );
       }
     }
